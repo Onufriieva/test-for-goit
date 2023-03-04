9b9ed1ed096867c151e13fc5345d962b71b05f11
Onufriieva <onufrieva.julia@gmail.com>
2023-03-04 18:50:55 +0200

    Розмістила фото та додала зміну follow

--- src/index.js
+++ src/index.js
@@ -1,45 +1,39 @@
-
-const STORAGE_KEY = "local";
- 
+const STORAGE_KEY = "local"; 
 
 const addButton = document.querySelector("button");
 const value = document.querySelector('#value');
+const followSpan = document.querySelector('#add')
 
 const counterValue = {
     value: 100500,
+    followSpan: "FOLLOW",
     add() {
         this.value += 1;
     },
+    change() {        
+        this.followSpan = "FOLLOWING";   
+    }
 };
-
-
-
-
-
-// addButton.addEventListener(`click`, (event) => {
-  
-//     addButton.classList.toggle(".is-active");
-//     counterValue.add();
-//     value.textContent = counterValue.value;
-  
-//     localStorage.setItem("STORAGE_KEY", JSON.stringify(counterValue.value));
-//     const savedValue = localStorage.getItem("STORAGE_KEY");
-//     const parsedValue = JSON.parse(savedValue);
-//     console.log(parsedValue)
-// });
-
-
-// addButton.addEventListener(`click`, (event) => {
-  
-//     addButton.classList.toggle("is-active");
-// });
 
 
 (() => {
     
     addButton.addEventListener("click", () => {
-      const expanded = addButton.getAttribute("aria-expanded") === "true" || false;
-      addButton.classList.add("is-active");
-      addButton.setAttribute("aria-expanded", !expanded);
-     });
+      const expanded = addButton.getAttribute("aria-expanded") === true || false;
+    if(!expanded) {
+        addButton.classList.add("is-active");
+        addButton.setAttribute("aria-expanded", !expanded);
+    };
+    
+    counterValue.add();
+    counterValue.change()
+    value.textContent = counterValue.value;
+    followSpan.textContent = counterValue.followSpan
+    localStorage.setItem("STORAGE_KEY", JSON.stringify(counterValue.value));
+    const savedValue = localStorage.getItem("STORAGE_KEY");
+    const parsedValue = JSON.parse(savedValue);
+    console.log(parsedValue)    
+    });
+    
   })();
+
